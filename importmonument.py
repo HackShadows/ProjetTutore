2e1450741a38175c3323c4925ae77f0bbf78bcea
p2302924 <francesco.ho-huu--campanelli@etu.univ-lyon1.fr>
2026-01-12 19:48:02 +0100

    script import qui doit marcher

--- importmonument.py
+++ importmonument.py
@@ -1,44 +1,47 @@
 import csv
 
 def Insert_SQL(line):
-	#output.write("INSERT INTO `Image` (`nom_image`,`url`,`nom_monument`,`description`,`nom_commune`,`geoloc`) VALUES ("+str()+","+str()+","+str()+","+str(line[12])+","+str(line[8])+","+str()+")")
-	#output.write(str(line) + '\n')
-	#output.write("INSERT INTO `Lieu` (`nom_commune`,`code_dept`,`nom_dept`,`nom_region`) VALUES ("+str(line[8])+","+str(line[3])+","+str()+","+str()+")")
-	#output.write(str(line) + '\n')
-	print(len(line))
+	output.write("INSERT INTO `Image` (`nom_image`,`url`,`nom_monument`,`description`,`nom_commune`,`geoloc`) VALUES ("+str(line[dico['nom_image']].split('/')[-1])+", "+str(line[dico['url']])+", "+str(line[dico['nom_monument']])+", "+str(line[dico['description']])+", "+str(line[dico['nom_commune']])+", "+str(line[dico['geoloc']])+")")
+	output.write('\n')
+	output.write("INSERT INTO `Lieu` (`nom_commune`,`code_dept`,`nom_dept`,`nom_region`) VALUES ("+str(line[dico['nom_commune']])+", "+str(line[dico['code_dept']])+", "+str(line[dico['nom_dept']])+", "+str(line[dico['nom_region']])+")")
+	output.write('\n\n')
 
 def splitline(line):
 	tab = []
+	skip = False
 	string = ""
 	for char in line :
-		if char == ';':
-			print(string)
+		if char == ';' and not skip:
 			if (string != ""):
 				tab.append(string)
 			else :
 				tab.append("NULL")
 			string = ""
-			
+		elif char == '"':
+			if skip:
+				skip = False
+			else:
+				skip = True
 		else :
 			string += char
+	if (string != ""):
+		tab.append(string)
+	else :
+		tab.append("NULL")
 	return tab
 
-#dico = {"nom_image":,"url":21,"nom_monument":,"description"12:,"nom_commune":8,"geoloc":19,"code_dept":3,"nom_dept":,"nom_region":24,}
+dico = {"nom_image" : 17, "url" : 17, "nom_monument" : 6, "description" : 12, "nom_commune" : 8, "geoloc" : 19, "code_dept" : 25, "nom_dept" : 26, "nom_region" : 24}
 with open('photographies_monuments.csv','r') as file:
 	with open('insert_in_db.sql', 'w') as output:
 		csvFile = csv.reader(file, delimiter=',')
 		limit = 0
+		firstline = True
 		for line in csvFile:
-			print(line)
-			line = ';'.join(line)
-			print(line)
+			if firstline:
+				firstline = False
+				continue
+			line = ','.join(line)
 			line = splitline(line)
-			for i in range(len(line)):
-				print(i,line[i])
-			print(line)
-			limit += 1
-			if limit == 2:
-				break	
-			#Insert_SQL(line)
+			Insert_SQL(line)
 
 		
