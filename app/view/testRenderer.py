297d8e924a79789783199c2075411d59504541e5
Cauchy Timothée <timothee.cauchy@etu.univ-lyon1.fr>
2026-01-21 22:46:50 +0100

    changements à l'architecture + ajout d'une page de création de puzzles

--- app/view/testRenderer.py
+++ app/view/testRenderer.py
@@ -2,15 +2,20 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import sys
 import webbrowser
 
-source = "templates/"
-destination = "renders/"
+with open("app_path.txt", "r") as path_file:
+	app_path = path_file.readline() # chemin de l'application
+
+source = "templates/" # le répertoire contenant les fichiers jinja
+template_extension = ".tmpl"
+destination = "renders/" # le répertoire destination
 
 environment = Environment(loader=FileSystemLoader(source), autoescape=select_autoescape(), trim_blocks=True)
 
-template = environment.get_template(sys.argv[1])
+template = environment.get_template(sys.argv[1] + template_extension)
 
-with open(destination + sys.argv[2], 'w') as file :
-    file.write(template.render())
+with open(destination + sys.argv[1] + ".html", 'w+') as file :
+	file.write(template.render(app_path = app_path))
 
-if len(sys.argv) > 3 and sys.argv[3]:
-    webbrowser.open(destination + sys.argv[2])
+# ouvre le navigateur si 2eme argument
+if len(sys.argv) > 2:
+	webbrowser.open(destination + sys.argv[1] + ".html")
