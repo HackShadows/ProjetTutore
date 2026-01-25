75ee0df8c371eb3ecb6a4c03a6ae83e7d9237423
valentin <valentin.laporte42@gmail.com>
2026-01-25 21:10:37 +0100

    images grandes mais écrasées

--- app/view/js/script-split-puzzle.js
+++ app/view/js/script-split-puzzle.js
@@ -6,12 +6,14 @@ function splitImage(url, puzzlesize, tilesize) {
 		img.onload = () => {
 			const sourceStepX = img.width / puzzlesize;
 			const sourceStepY = img.height / puzzlesize;
+			console.log(`taille de sourceStep : ${sourceStepY}`);
 			for (let row = 0; row < puzzlesize; row++) {
 				for (let col = 0; col < puzzlesize; col++) {
 					const canvas = document.createElement("canvas");
 					canvas.width = tilesize;
 					canvas.height = tilesize;
 					const context = canvas.getContext("2d");
+					console.log(`taille du canva : ${tilesize}`)
 					context.drawImage(
 						img,
 						col * sourceStepX, row * sourceStepY, // Source X, Y (début de la coupe)
@@ -21,10 +23,7 @@ function splitImage(url, puzzlesize, tilesize) {
 					);
 					tiles.push({
 						id: row * puzzlesize + col,
-						img: canvas.toDataURL(), // L'image sera redimensionnée à pieceSize x pieceSize
-						rotation: 0,
-						correctPos: {row: row, col: col},
-						currentPos: {row: row, col: col}
+						img: canvas.toDataURL(),
 					});
 				}
 			}
@@ -34,7 +33,6 @@ function splitImage(url, puzzlesize, tilesize) {
 			console.error("Erreur chargement image", err);
 			reject(err);
 		};
-
 		img.src = url;
 	});
 }
@@ -43,10 +41,11 @@ async function demarrerJeu() {
 	const url = document.getElementById('imageurl').value;
 	const puzzlesize = document.getElementById('imagesize').value;
 	// const tilesize = document.getElementById('cellsize').value;
+	const tilesize = 300;
 
 	if (url) {
 		try {
-			const tiles = await splitImage(url, puzzlesize, 150);
+			const tiles = await splitImage(url, puzzlesize, tilesize);
 			console.log("Pièces générées :", tiles);
 			for (let i = 0; i < puzzlesize * puzzlesize; i++) {
 				document.getElementById("tile" + i).src = tiles[i]['img'];
