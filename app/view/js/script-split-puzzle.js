b2575b53544c4475bcdd9846d26a532db7f8b074
valentin <valentin.laporte42@gmail.com>
2026-01-25 20:46:27 +0100

    affichage de mini tuile, je travaille pour les agrandir

--- app/view/js/script-split-puzzle.js
+++ app/view/js/script-split-puzzle.js
@@ -1,27 +1,61 @@
 function splitImage(url, puzzlesize, tilesize) {
-    const img = new Image();
-    img.src = url;
-    let tiles_url = [];
-    height = tilesize;
-    width = tilesize;
-    for(var x = 0; x < puzzlesize; ++x) {
-        for(var y = 0; y < puzzlesize; ++y) {
-            var canvas = document.createElement('canvas');
-            canvas.width = width;
-            canvas.height = height;
-            var context = canvas.getContext('2d');
-            context.drawImage(img, x * width, y * height, width, height, 0, 0, canvas.width, canvas.height);
-            tiles_url.push(canvas.toDataURL());
-        }
-    }
-    return tiles_url;
+	return new Promise((resolve, reject) => {
+		const tiles = [];
+		const img = new Image();
+		img.crossOrigin = "Anonymous";
+		img.onload = () => {
+			const sourceStepX = img.width / puzzlesize;
+			const sourceStepY = img.height / puzzlesize;
+			for (let row = 0; row < puzzlesize; row++) {
+				for (let col = 0; col < puzzlesize; col++) {
+					const canvas = document.createElement("canvas");
+					canvas.width = tilesize;
+					canvas.height = tilesize;
+					const context = canvas.getContext("2d");
+					context.drawImage(
+						img,
+						col * sourceStepX, row * sourceStepY, // Source X, Y (début de la coupe)
+						sourceStepX, sourceStepY,             // Source W, H (taille de la coupe)
+						0, 0,                                 // Destination X, Y
+						tilesize, tilesize                  // Destination W, H (redimensionnement force)
+					);
+					tiles.push({
+						id: row * puzzlesize + col,
+						img: canvas.toDataURL(), // L'image sera redimensionnée à pieceSize x pieceSize
+						rotation: 0,
+						correctPos: {row: row, col: col},
+						currentPos: {row: row, col: col}
+					});
+				}
+			}
+			resolve(tiles);
+		};
+		img.onerror = (err) => {
+			console.error("Erreur chargement image", err);
+			reject(err);
+		};
+
+		img.src = url;
+	});
 }
 
-url = document.getElementById('imageurl').value;
-puzzlesize = document.getElementById('imagesize').value;
-tilesize = document.getElementById('cellsize').value;
-tiles = splitImage(url,puzzlesize,cellsize);
-for (let i = 0 ; i < size*size ; i++) {
-    document.getElementById("tile"+i).src = tiles [i];
+async function demarrerJeu() {
+	const url = document.getElementById('imageurl').value;
+	const puzzlesize = document.getElementById('imagesize').value;
+	// const tilesize = document.getElementById('cellsize').value;
+
+	if (url) {
+		try {
+			const tiles = await splitImage(url, puzzlesize, 150);
+			console.log("Pièces générées :", tiles);
+			for (let i = 0; i < puzzlesize * puzzlesize; i++) {
+				document.getElementById("tile" + i).src = tiles[i]['img'];
+			}
+			console.log("done")
+		} catch (error) {
+			console.error("Problème de génération du puzzle", error);
+		}
+	}
 }
-console.log("done");
+
+demarrerJeu()
